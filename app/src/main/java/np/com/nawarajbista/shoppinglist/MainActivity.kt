ff87a55fba211b705c1870aa6654ff0ec592e40b
nawadeveloper <nawarajusa@gmail.com>
2019-07-24 12:29:11 -0400

    room database completed

--- app/src/main/java/np/com/nawarajbista/shoppinglist/MainActivity.kt
+++ app/src/main/java/np/com/nawarajbista/shoppinglist/MainActivity.kt
@@ -5,19 +5,32 @@ import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.widget.Toast
+import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.ViewHolder
 import kotlinx.android.synthetic.main.activity_main.*
 import np.com.nawarajbista.shoppinglist.data.AppDatabase
 import np.com.nawarajbista.shoppinglist.data.ShoppingList
+import np.com.nawarajbista.shoppinglist.viewHolder.ShoppingListGroup
 
 class MainActivity : AppCompatActivity() {
 
-    var num: Int = 1
+    private var num: Int = 1
+    private val adapter = GroupAdapter<ViewHolder>()
+    private var db: AppDatabase? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         num = editText_number.text.toString().toInt()
+
+        db = AppDatabase.getAppDataBase(this)
+
+
+        addListToAdapter()
+
+        list_view.adapter = adapter
 
 
         button_add.setOnClickListener {
@@ -39,6 +52,9 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+
+
+
     private fun reduceNumber() {
         if(num > 1) {
             num--
@@ -54,12 +70,12 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun addDataToDatabase(item_count:Int, item_name: String) {
-
-        val db = AppDatabase.getAppDataBase(this) ?: return
+//        db = AppDatabase.getAppDataBase(this) ?: return
+        if(db == null) return
         val shoppingList = ShoppingList(item_count, item_name)
 
         Thread {
-            db.shoppingDao().insertAll(shoppingList)
+            db?.shoppingDao()?.insertAll(shoppingList)
 
             // included this for future reference
             // this shows how we can use handler looper to communicate with main thread
@@ -67,17 +83,35 @@ class MainActivity : AppCompatActivity() {
             val runnable = Runnable {
                 run {
                     Toast.makeText(this, "list added", Toast.LENGTH_SHORT).show()
-                    editText_name.setText(db.shoppingDao().getAll().toString())
+                    editText_name.setText("")
                     editText_number.setText("1")
+
                 }
             }
 
             handler.post(runnable)
 
         }.start()
+    }
 
+    private fun addListToAdapter() {
+        if(db == null) {
+            Toast.
+                makeText(
+                    this,
+                    "could not connect to data base",
+                    Toast.LENGTH_SHORT
+                ).show()
+        } else {
 
+            val allData = db?.shoppingDao()?.getAll()
 
-        AppDatabase.destroyInstance()
+            allData?.observe(this, android.arch.lifecycle.Observer {
+                adapter.clear()
+                it?.forEach {list ->
+                    adapter.add(ShoppingListGroup(list))
+                }
+            })
+        }
     }
 }
---
+++ app/src/main/java/np/com/nawarajbista/shoppinglist/viewHolder/ShoppingListGroup.kt
@@ -0,0 +1,20 @@
+package np.com.nawarajbista.shoppinglist.viewHolder
+
+import com.xwray.groupie.Item
+import com.xwray.groupie.ViewHolder
+import kotlinx.android.synthetic.main.shopping_row.view.*
+import np.com.nawarajbista.shoppinglist.R
+import np.com.nawarajbista.shoppinglist.data.ShoppingList
+
+class ShoppingListGroup(private val sp: ShoppingList): Item<ViewHolder>() {
+    override fun bind(viewHolder: ViewHolder, position: Int) {
+
+        viewHolder.itemView.textView_quantities.text = sp.quantities.toString()
+        viewHolder.itemView.textView_name.text = sp.itemName
+
+    }
+
+    override fun getLayout(): Int {
+        return R.layout.shopping_row
+    }
+}
